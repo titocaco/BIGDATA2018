dd5d24cf72b227044a29c5a9e240dbac6bcb5561
titocaco <titocaco.jazz@gmail.com>
2018-03-03 12:41:14 -0300

    Hello, functional world!

---
+++ Hello.hs
@@ -0,0 +1,13 @@
+--
+-- Copyright (c) 2017 Nome - site
+-- GPL version 3 or later
+-- (see http://www.gnu.org/copyleft/gpl.html)
+--
+
+-- | Main module
+module Main where
+
+-- | Main function
+main :: IO ()
+main = do
+    print ("Hello World")
