10acc6aa0613c2bee34af9a86874da707d779fae
titocaco <titocaco.jazz@gmail.com>
2018-03-04 18:21:57 -0300

    Atividade 1 - Exercício 6

---
+++ ATIVIDADE1/EXERCICIO_06.hs
@@ -0,0 +1,12 @@
+-- | Main module
+module Main where
+
+-- | div2d function
+div2d :: Integer -> Double
+div2d x = fromIntegral x / 2
+
+-- | Main function
+main = do
+    print (div2d 1)
+    print (div2d 2)
+    print (div2d 3)
