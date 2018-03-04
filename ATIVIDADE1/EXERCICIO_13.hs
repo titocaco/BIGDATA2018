f56587baa961b04f8a2843c38218cda8440fcc44
titocaco <titocaco.jazz@gmail.com>
2018-03-04 19:57:13 -0300

    Atividade 1 - Exercício 13

---
+++ ATIVIDADE1/EXERCICIO_13.hs
@@ -0,0 +1,12 @@
+-- | Main module
+module Main where
+
+-- | Package imports
+import Data.Char
+
+-- | string2list function
+string2list s = map digitToInt s
+
+-- | Main function
+main = do
+    print $ string2list "0123456789"
