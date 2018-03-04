87f2390329c4f2de941d1b8c18f2d20ee08a4bb2
titocaco <titocaco.jazz@gmail.com>
2018-03-04 18:21:41 -0300

    Atividade 1 - Exercício 4

---
+++ ATIVIDADE1/EXERCICIO_04.hs
@@ -0,0 +1,12 @@
+-- | Main module
+module Main where
+
+-- | mult35 function
+mult35 :: Int -> Bool
+mult35 x = x `mod` 3 == 0 && x `mod` 5 == 0
+
+-- | Main function
+main = do
+    print (mult35 15)
+    print (mult35 6)
+    print (mult35 10)
