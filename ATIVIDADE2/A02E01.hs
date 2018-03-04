fccd66ed0f266d1a1e6369e8edfe8e0ff6d962d6
titocaco <titocaco.jazz@gmail.com>
2018-03-04 20:11:55 -0300

    Atividade 2 - Exercício 1

---
+++ ATIVIDADE2/A02E01.hs
@@ -0,0 +1,11 @@
+-- | Main module
+module Main where
+
+-- ehTriangulo function
+ehTriangulo :: Int -> Int -> Int -> Bool
+ehTriangulo a b c = a+b > c && a+c > b && b+c > a
+
+-- | Main function
+main = do
+    print $ ehTriangulo 3 4 5
+    print $ ehTriangulo 3 4 10
