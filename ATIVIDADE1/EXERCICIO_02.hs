75e6ed1f62fdb43f23cc11587c36d6228593d7b4
titocaco <titocaco.jazz@gmail.com>
2018-03-04 18:20:34 -0300

    Atividade 1 - Exercício 2

---
+++ ATIVIDADE1/EXERCICIO_02.hs
@@ -0,0 +1,11 @@
+-- | Main module
+module Main where
+
+-- | mult3 function
+mult3 :: Int -> Bool
+mult3 x = x `mod` 3 == 0
+
+-- | Main function
+main = do
+    print (mult3 3)
+    print (mult3 4)
