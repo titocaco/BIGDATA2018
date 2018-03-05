36c14aa0ef7ad874e28e185a037ac921ad3c15f4
titocaco <titocaco.jazz@gmail.com>
2018-03-04 21:40:20 -0300

    Atividade 2 - Exercício 2

---
+++ ATIVIDADE2/A02E02.hs
@@ -0,0 +1,15 @@
+-- | Main module
+module Main where
+
+-- | tipoTriangulo function
+tipoTriangulo :: Int -> Int -> Int -> String
+tipoTriangulo a b c
+    | a == b && a == c = "Equilateral"
+    | a == b || a == c || b == c = "Isosceles"
+    | otherwise = "Scalene"
+
+-- | Main function
+main = do
+    print $ tipoTriangulo 3 4 5
+    print $ tipoTriangulo 3 3 5
+    print $ tipoTriangulo 3 3 3
