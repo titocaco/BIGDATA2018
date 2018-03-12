c0f95670b92a2c7a1819f8e7e8f16df3b7ea054a
titocaco <titocaco.jazz@gmail.com>
2018-03-11 23:38:06 -0300

    Adding some headers (comments) to L02E05

--- ATIVIDADE1/LISTA 2 - FUNÇÕES/L02E05.hs
+++ ATIVIDADE1/LISTA 2 - FUNÇÕES/L02E05.hs
@@ -1,9 +1,11 @@
 -- | Main module
 module Main where
 
+-- | digitos function
 digitos :: Int -> [Int]
 digitos d = [read [x] | x <- show d]
 
+-- | somaDigitos function
 somaDigitos :: Int -> Int
 somaDigitos = sum . digitos
 
