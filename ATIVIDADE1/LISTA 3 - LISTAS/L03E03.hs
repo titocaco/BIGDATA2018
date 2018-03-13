51832dd2406964dfdab75d26496eea1888277cd4
titocaco <titocaco.jazz@gmail.com>
2018-03-13 17:44:49 -0300

    More straight and simple code in L03E03

--- ATIVIDADE1/LISTA 3 - LISTAS/L03E03.hs
+++ ATIVIDADE1/LISTA 3 - LISTAS/L03E03.hs
@@ -2,10 +2,8 @@
 module Main where
 
 -- | fibonacci function
-fibonacci n = take n $ fibo 1 1
-    where
-        fibo x y = x:fibo y (x + y)
+fibonacci x y = x:fibonacci y (x + y)
 
 -- | Main function
 main = do
-    print $ fibonacci 10
+    print $ take 10 $ fibonacci 1 1
