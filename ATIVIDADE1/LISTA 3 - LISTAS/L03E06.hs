49195b3ad15a3b489e670ae1589cb25a742ac675
titocaco <titocaco.jazz@gmail.com>
2018-03-13 18:30:32 -0300

    Atividade 1 - Lista 3 - Exercício 6

---
+++ ATIVIDADE1/LISTA 3 - LISTAS/L03E06.hs
@@ -0,0 +1,12 @@
+-- | Main module
+module Main where
+
+-- | collatz function
+collatz :: Integer -> Integer
+collatz x
+    | x `mod` 2 == 0 = x `div` 2
+    | otherwise = 3 * x + 1
+
+-- | Main function
+main = do
+    print $ collatz 6
