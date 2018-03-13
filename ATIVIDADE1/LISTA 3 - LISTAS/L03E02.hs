bf8ad74a9f154a5f5ce875b1043137a700b6a111
titocaco <titocaco.jazz@gmail.com>
2018-03-13 13:41:14 -0300

    Adding some comments to L03E02 function

--- ATIVIDADE1/LISTA 3 - LISTAS/L03E02.hs
+++ ATIVIDADE1/LISTA 3 - LISTAS/L03E02.hs
@@ -2,6 +2,9 @@
 module Main where
 
 -- | projectEuler5 function
+-- Applies the lcm (least common multiplier) to the two last elements of the list [1..20],
+-- then with the previous element of the list and the result of the last iteration,
+-- and keep this algorithm along the rest of the list.
 projectEuler5 :: Integer
 projectEuler5 = foldr1 lcm [1..20]
 
