48b011f670de29606988b2a162320acaf55ee295
titocaco <titocaco.jazz@gmail.com>
2018-03-13 13:27:16 -0300

    Using a different method in L03E01

--- ATIVIDADE1/LISTA 3 - LISTAS/L03E01.hs
+++ ATIVIDADE1/LISTA 3 - LISTAS/L03E01.hs
@@ -2,9 +2,13 @@
 module Main where
 
 -- | divisivel20 function
-divisivel20 :: Int -> Bool
-divisivel20 n = and [n `mod` x == 0 | x <- [1..20]]
+divisivel20 :: Integer -> Bool
+divisivel20 n = n `mod` base == 0
+    where
+        base = foldr1 lcm [1..20]
 
 -- | Main function
 main = do
     print $ divisivel20 2432902008176640
+    print $ divisivel20 1337
+    print $ divisivel20 232792560
