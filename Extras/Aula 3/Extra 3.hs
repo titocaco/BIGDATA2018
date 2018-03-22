3305fa3c3071a48892e21e9bed234e259bb7e193
titocaco <titocaco.jazz@gmail.com>
2018-03-22 19:02:13 -0300

    Adding extra exercise 3 from Aula 3

---
+++ Extras/Aula 3/Extra 3.hs
@@ -0,0 +1,21 @@
+{-
+    Aula:       3
+    Exercício:  3
+    Enunciado:  Implemente fibonacci em paralelo
+-}
+
+-- | Main module
+module Main where
+
+    import Control.Parallel
+
+    fib :: Integer -> Integer
+    fib 0 = 0
+    fib 1 = 1
+    fib n = n1 `par` n2 `pseq` (n1 + n2)
+        where
+            n1 = fib (n - 1)
+            n2 = fib (n - 2)
+
+    main = do
+        print $ fib 40
